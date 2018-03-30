9441a6220c41a34875c42fd025e897c4e8a10072
Akberovr <a.rovshan@code.edu.az>
2018-03-30 21:22:01 +0400

    performing file upload using google drive api to google spreadsheet starts

--- App/Controllers/Question.php
+++ App/Controllers/Question.php
@@ -1,63 +1,51 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-
 namespace App\Controllers;
+
 use \Core\View;
 use App\Models\Question as QuestionModel;
 
+class Question extends \Core\Controller {
 
-class Question  extends \Core\Controller
-{
-    
-    
-     
-    
     public function indexAction() {
-         
-          $redirect = "http://localhost/sgi-google-sheets-api-append/tutorial-google-oauth-callback.php"; // Enter your API Callback URL here
-            $client_id = "931826885258-qb5lff27f79l3s66ufriebi2v1sdc0sf.apps.googleusercontent.com"; // Enter your API Client ID Here
-            $client_secret = "SWriR1bV0_Yh8Vmp5l4sONBb"; // Enter your API Client Secret here
+// Config
+$redirect = "http://localhost/sgi-google-sheets-api-append/tutorial-google-oauth-callback.php"; // Enter your API Callback URL here
+$client_id = "914656398258-g6evcoag7ihcqqurkt1scne07gu9a5kf.apps.googleusercontent.com"; // Enter your API Client ID Here
+ $client_secret = "Aj06U52b1ZNtMPvSTKrzJkOS"; // Enter your API Client Secret here
+// Check Code
+if(empty($_GET['code'])) {
+	// Authorization Link
+	$authorization = "https://accounts.google.com/o/oauth2/auth?redirect_uri=$redirect&client_id=$client_id&response_type=code&scope=https://www.googleapis.com/auth/spreadsheets&approval_prompt=force&access_type=offline";
+	
+	} else {
+	// Authorization
+	$code = $_GET['code'];	
+	// Token
+	$url = "https://accounts.google.com/o/oauth2/token";
+	$data = "code=$code&client_id=$client_id&client_secret=$client_secret&redirect_uri=$redirect&grant_type=authorization_code";	
+	// Request
+	$ch = @curl_init();
+	@curl_setopt($ch, CURLOPT_POST, true);
+	@curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	@curl_setopt($ch, CURLOPT_URL, $url);
+	@curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/x-www-form-urlencoded'
+	));
+	@curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response = @curl_exec($ch); 
+	$status_code = @curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+	@curl_close($ch);
+	$array = json_decode($response);
+	
+}
 
-            if (empty($_GET['code'])) {
-                // Authorization Link
-                $authorization = "https://accounts.google.com/o/oauth2/auth?redirect_uri=$redirect&client_id=$client_id&response_type=code&scope=https://www.googleapis.com/auth/spreadsheets&approval_prompt=force&access_type=offline";
-
-            } else {
-                // Authorization
-                $code = $_GET['code'];
-                // Token
-                $url = "https://accounts.google.com/o/oauth2/token";
-                $data = "code=$code&client_id=$client_id&client_secret=$client_secret&redirect_uri=$redirect&grant_type=authorization_code";
-                // Request
-                $ch = @curl_init();
-                @curl_setopt($ch, CURLOPT_POST, true);
-                @curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-                @curl_setopt($ch, CURLOPT_URL, $url);
-                @curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                            'Content-Type: application/x-www-form-urlencoded'
-                ));
-                @curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $response = @curl_exec($ch);
-                $status_code = @curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                @curl_close($ch);
-                $array = json_decode($response);
-                print_r($array);
-                
-                }
         View::renderTemplate('Question/index.html');
-        
-            
     }
-    
-    
-    
-    
-    
-    
+
 }
